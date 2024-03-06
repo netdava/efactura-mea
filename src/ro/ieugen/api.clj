09127d821ca7bbbb11032aedf78ee393da294d08
Andrei <sandre1>
2024-03-06 18:50:42 +0200

    mutat url-uri din config, functie pentru build-url :test sau :prod

--- src/ro/ieugen/api.clj
+++ src/ro/ieugen/api.clj
@@ -1,7 +1,8 @@
 (ns ro.ieugen.api
   (:require [babashka.http-client :as http]
             [clojure.java.io :as io]
-            [jsonista.core :as j]))
+            [jsonista.core :as j]
+            [ro.ieugen.oauth2-anaf :refer [make-query-string]]))
 
 (defn get-access-token [name]
   (let [env (System/getenv)
@@ -18,41 +19,63 @@
      :prod "https://api.anaf.ro/prod/FCTEL/rest/descarcare?id=%s"}}
    :cif "35586426"})
 
-(defn save-zip-file [response file-path]
+
+(defn build-url
+  "Build a url from a base and a opts-map {:endpoint <type>}
+   - <type> can be :prod or :test;"
+  [url-base opts]
+  (let [type (:endpoint opts)]
+    (format url-base (name type))))
+
+(comment
+  (build-url "https://api.anaf.ro/%s/FCTEL/rest/" {:endpoint :test}))
+
+(defn save-zip-file [data file-path]
   (let [f (io/file file-path)
         _ (doto (-> (.getParentFile f)
                     (.mkdirs)))]
     (with-open [output-stream (io/output-stream file-path)]
-      (io/copy (io/input-stream (:body response)) output-stream))))
+      (io/copy (io/input-stream data) output-stream))))
 
 (defn obtine-lista-facturi 
   "Obtine lista de facturi pe o perioada de 60 zile din urmă;
-   - funcționează doar cu endpointurile de test/prod :lista-mesaje;
-   - <environment> can be :prod or :test ."
-  [environment]
-  (let [a-token (get-access-token "EFACTURA_ACCESS_TOKEN")
-        headers {:headers {"Authorization" (str "Bearer " a-token)}}
-        url (get-in config [:endpoint :lista-mesaje environment])
-        days "60"
-        cif (:cif config)
-        endpoint (format url days cif)
-        r (http/get endpoint headers)
-        body (:body r)
-        object-mapper (j/object-mapper {:decode-key-fn true})
-        lista-facturi (j/read-value body object-mapper)]
-    lista-facturi))
+   - apeleaza mediul de :test din oficiu;
+   - primeste un map {:endpoint <type>}, <type> poate fi :prod sau :test ."
+  ([]
+   (obtine-lista-facturi {:endpoint :test}))
+  ([opts]
+   (let [a-token (get-access-token "EFACTURA_ACCESS_TOKEN")
+         headers {:headers {"Authorization" (str "Bearer " a-token)}}
+         format-url "https://api.anaf.ro/%s/FCTEL/rest/listaMesajeFactura"
+         base-url (build-url format-url opts)
+         cif (:cif config)
+         q-str {"zile" "60"
+                "cif" cif}
+         endpoint (str base-url "?" (make-query-string q-str))
+         r (http/get endpoint headers)
+         body (:body r)
+         object-mapper (j/object-mapper {:decode-key-fn true})
+         lista-facturi (j/read-value body object-mapper)]
+     lista-facturi)))
+
+(comment
+  (obtine-lista-facturi {:endpoint :prod}))
 
 (defn descarca-factura
   "Descarcă factura în format zip pe baza id-ului.
-   - funcționează doar cu endpointurile de test/prod :lista-mesaje;
-   - <environment> can be :prod or :test ."
-  [id download-to environment]
+   - funcționează doar cu endpointurile de test/prod;
+   - opts un map {:endpoint <type>}, <type> poate fi :prod sau :test ."
+  [id download-to opts]
   (let [a-token (get-access-token "EFACTURA_ACCESS_TOKEN")
         headers {:headers {"Authorization" (str "Bearer " a-token)} :as :stream}
-        url (get-in config [:endpoint :descarcare environment])
-        endpoint (format url id)
-        response (http/get endpoint headers)]
-    (save-zip-file response (str download-to "/" id ".zip"))))
+        format-url "https://api.anaf.ro/%s/FCTEL/rest/descarcare"
+        base-url(build-url format-url opts)
+        endpoint (str base-url "?id=" id)
+        response (http/get endpoint headers)
+        data (:body response)
+        file-path (str download-to "/" id ".zip")]
+    (save-zip-file data file-path)
+    (println "Am descărcat" (str id ".zip") "pe calea" file-path)))
 
 (defn list-files-from-dir [dir]
   (let [directory (clojure.java.io/file dir)
@@ -71,5 +94,6 @@
 
   (obtine-lista-facturi :prod)
 
-  (descarca-factura "3182888140" "my-files/abc" :prod)
+  (descarca-factura "3182888140" "my-files/abc" {:endpoint :test})
+  
   )
