2ca6b015e3f6f68aa5625955d6491cc694db27a2
Andrei <sandre1>
2024-03-07 15:26:32 +0200

    functie sa verifice existenta si sa descarce facturi

--- src/ro/ieugen/api.clj
+++ src/ro/ieugen/api.clj
@@ -2,8 +2,13 @@
   (:require [babashka.http-client :as http]
             [clojure.java.io :as io]
             [jsonista.core :as j]
-            [ro.ieugen.oauth2-anaf :refer [make-query-string]])
+            [ro.ieugen.oauth2-anaf :refer [make-query-string]]
+            [next.jdbc :as jdbc])
   (:import [java.util.zip ZipFile]))
+
+(def db-spec {:dbtype "sqlite"
+              :dbname "facturi-anaf.db"})
+(def ds (jdbc/get-datasource db-spec))
 
 (defn get-access-token [name]
   (let [env (System/getenv)
@@ -27,7 +32,7 @@
     (with-open [output-stream (io/output-stream file-path)]
       (io/copy (io/input-stream data) output-stream))))
 
-(defn obtine-lista-facturi 
+(defn obtine-lista-facturi
   "Obtine lista de facturi pe o perioada de 60 zile din urmă;
    - apeleaza mediul de :test din oficiu;
    - primeste un map {:endpoint <type>}, <type> poate fi :prod sau :test ."
@@ -48,6 +53,14 @@
          lista-facturi (j/read-value body object-mapper)]
      lista-facturi)))
 
+(defn scrie-factura->db [factura]
+  (let [{:keys [id data_creare tip cif id_solicitare detalii]} factura
+        q "INSERT INTO facturi_anaf
+           (abstract_id, data_creare, tip, cif, id_solicitare, detalii)
+           VALUES (?, ?, ?, ?, ?, ?)"]
+    (jdbc/execute! ds [q id data_creare tip cif id_solicitare detalii])))
+
+
 (comment
   (obtine-lista-facturi {:endpoint :prod}))
 
@@ -59,7 +72,7 @@
   (let [a-token (get-access-token "EFACTURA_ACCESS_TOKEN")
         headers {:headers {"Authorization" (str "Bearer " a-token)} :as :stream}
         format-url "https://api.anaf.ro/%s/FCTEL/rest/descarcare"
-        base-url(build-url format-url opts)
+        base-url (build-url format-url opts)
         endpoint (str base-url "?id=" id)
         response (http/get endpoint headers)
         data (:body response)
@@ -67,55 +80,48 @@
     (save-zip-file data file-path)
     (println "Am descărcat" (str id ".zip") "pe calea" file-path)))
 
-(defn list-files-from-dir [dir]
+#_(defn list-files-from-dir [dir]
   (let [directory (clojure.java.io/file dir)
         dir? #(.isDirectory %)]
     (map #(.getName %) ;; .getPath?!
          (filter (comp not dir?)
                  (tree-seq dir? #(.listFiles %) directory)))))
 
-(defn is-file-in-dir? [file dir]
+#_(defn is-file-in-dir? [file dir]
   (let [files (list-files-from-dir dir)]
     (some #(= file %) files)))
+#_(defn entries [zipfile]
+  (enumeration-seq (.entries zipfile)))
 
+#_(defn walkzip [fileName]
+  (with-open [z (ZipFile. fileName)]
+    (doseq [e (entries z)]
+      (println (.getName e)))))
 
+(defn build-path [data-creare]
+  (let [an (subs data-creare 0 4)
+        luna (subs data-creare 4 6)]
+    (str an "/" luna)))
+
+(defn download-zip-file [factura opts]
+  (let [{:keys [id data_creare]} factura
+        date-path (build-path data_creare)
+        path (str "facturi/" date-path)]
+    (descarca-factura id path opts)))
+
+(defn verifica-descarca-facturi [opts]
+  (let [l (obtine-lista-facturi opts)
+        facturi (:mesaje l)]
+    (doseq [f facturi]
+      (let [id (:id f)
+            zip-name (str id ".zip")
+            test-file-exist (jdbc/execute! ds ["select id from facturi_anaf where
+                      abstract_id = ? " id])]
+        (if (empty? test-file-exist)
+          (do (download-zip-file f opts)
+              (scrie-factura->db f))
+          (println "factura" zip-name "exista salvata local"))))))
 
 (comment
-  (defn entries [zipfile]
-    (enumeration-seq (.entries zipfile)))
-
-  (defn walkzip [fileName]
-    (with-open [z (ZipFile. fileName)]
-      (doseq [e (entries z)]
-        (println (.getName e)))))
-
-  (is-file-in-dir? "3192491497.zip" "facturi-descarcate-zip")
-
-
-  (descarca-factura "3182888140" "my-files/abc" {:endpoint :prod})
-
-  (defn build-path [data-creare]
-    (let [an (subs data-creare 0 4)
-          luna (subs data-creare 4 6)]
-      (str an "/" luna)))
-
-  (defn download-zip-file [factura opts]
-    (let [{:keys [id data_creare]} factura
-          date-path (build-path data_creare)
-          path (str "facturi/" date-path)]
-      (descarca-factura id path opts)))
-
-  (defn verifica-descarca-facturi [opts]
-    (let [l (obtine-lista-facturi opts)
-          facturi (:mesaje l)
-          local-dir "facturi"]
-      (doseq [f facturi]
-        (let [zip-name (str (:id f) ".zip")]
-          (if (is-file-in-dir? zip-name local-dir)
-            (println "factura" zip-name "exista salvata local")
-            (download-zip-file f opts))))))
-  
   (verifica-descarca-facturi {:endpoint :prod})
-  
-
   )
