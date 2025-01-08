05b575be8e670e016cff42d38763da8365140901
Eugen Stan <eugen.stan@netdava.com>
2025-01-08 11:59:08 +0200

    Ceva modificări
    * ExecutorService sunt acum stări mount + fac shutdown pe :stop
    * Folosim tools logging în loc de println
    * ceva redenumire de ns-uri anaf

--- src/efactura_mea/db/db_ops.clj
+++ src/efactura_mea/db/db_ops.clj
@@ -1,9 +1,10 @@
-(ns efactura-mea.db.db-ops
-  (:require [efactura-mea.db.facturi :as f]
-            [efactura-mea.util :as u]
-            [java-time.api :as jt]
-            [next.jdbc :as jdbc]
-            [clojure.tools.logging :refer [info]]))
+(ns efactura-mea.db.db-ops 
+  (:require
+   [clojure.tools.logging :refer [info]]
+   [efactura-mea.db.facturi :as f]
+   [efactura-mea.util :as u]
+   [java-time.api :as jt]
+   [next.jdbc :as jdbc]))
 
 (defn enable-foreignkey-constraint [spec]
   ;; Enable foreign key constraints
@@ -78,9 +79,29 @@
 
 (defn db-config
   [ds]
+<<<<<<< HEAD
   (info "Enabling foreign-key constraint, convert to WAL mode")
   (db-init-pref ds)
   (info "Set :automated-proc-status to off for companies")
+=======
+  (log/debug "** Enabling foreign-key constraint, convert to WAL mode")
+  (db-init-pref ds)
+  (log/debug "** Creating table lista_mesaje")
+  (f/create-facturi-anaf-table ds)
+  (log/debug "** Creating table detalii_facturi_anaf")
+  (f/create-detalii-facturi-anaf-table ds)
+  (log/debug "** Creating table company")
+  (f/create-company-table ds)
+  (log/debug "** Creating table tokens")
+  (f/create-tokens-table ds)
+  (log/debug "** Creating table apeluri_api_anaf")
+  (f/create-apeluri-api-anaf ds)
+  (log/debug "** Creating table descarcare_lista_mesaje")
+  (f/create-descarcare-lista-mesaje ds)
+  (log/debug "** Creating table company_automated_proc")
+  (f/create-automated-processes-table ds)
+  (log/debug "** Set :automated-proc-status to off for companies")
+>>>>>>> b43614d (Ceva modificări)
   (init-automated-download ds))
 
 (defn scrie-factura->db [factura ds]
