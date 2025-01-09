e6d9a630959815c8d09807c6e8d66f9ea27c01d6
Eugen Stan <eugen.stan@netdava.com>
2025-01-10 01:08:09 +0200

    Carduri pe prima pagină

--- src/efactura_mea/web/home.clj
+++ src/efactura_mea/web/home.clj
@@ -1,11 +1,14 @@
 (ns efactura-mea.web.home
-  (:require [efactura-mea.web.layout :as layout]))
+  (:require 
+   [efactura-mea.web.layout :as layout]))
 
 (defn handle-homepage
   [req]
   (let [{:keys [headers]} req
         {:strs [hx-request]} headers
-        content {:status 200 :body "Bine ai venit pe eFacturaMea" :headers {"content-type" "text/html"}}
+        content {:status 200
+                 :body "Bine ai venit pe eFacturaMea"
+                 :headers {"content-type" "text/html"}}
         sidebar (layout/sidebar-select-company)]
     (if hx-request
       content
